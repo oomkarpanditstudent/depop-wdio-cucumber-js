3e11d898589c911e56841b8e33a1ab07ff1b4c7a
Oomkar Pandit <oomkar.pandit@royalmail.com>
2021-12-02 17:41:30 +0000

    out of box - successfull set up and execution

--- wdio.conf.js
+++ wdio.conf.js
@@ -132,7 +132,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec','cucumber'],
+    reporters: ['spec'],
 
 
     //
