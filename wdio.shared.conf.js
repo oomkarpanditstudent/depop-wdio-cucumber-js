251c7ab6a0e3a822e0045edafbad21cdf389da12
Oomkar Pandit <oomkar.pandit@royalmail.com>
2021-12-04 14:36:21 +0000

    Expanded framework, added different execution options - local chrome, execution in docker (without npm install) and execution in docker with npm install i.e. in case of jenkins setup

--- wdio.shared.conf.js
+++ wdio.shared.conf.js
@@ -52,20 +52,20 @@ exports.config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://saucelabs.com/platform/platform-configurator
     //
-    capabilities: [{
+    // capabilities: [{
     
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
-        maxInstances: 5,
+        //maxInstances: 5,
         //
-        browserName: 'chrome',
-        acceptInsecureCerts: true
+        //browserName: 'chrome',
+        //acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
         // excludeDriverLogs: ['bugreport', 'server'],
-    }],
+    //}],
     //
     // ===================
     // Test Configurations
@@ -113,7 +113,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+   
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
